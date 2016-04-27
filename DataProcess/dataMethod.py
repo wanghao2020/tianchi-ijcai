e63ec55f118eec43da0d06e2d0f58849a980c1f5
goiter <wgf1109@qq.com>
2016-04-27 21:53:52 +0800

    method

--- DataProcess/dataMethod.py
+++ DataProcess/dataMethod.py
@@ -1,21 +1,26 @@
-import MySQLdb
-
+#import MySQLdb
+import numpy as np
+import csv
 # class for the data processing
 
 class data :
 
     # file path
-    file_path = '/home/wanghao/Document/tianchi/data_sets/'
-    taobao_path = file_path + 'ijcai2016_taobao'
-    merchantinfo_path = file_path + 'ijcai2016_merchant_info'
-    koubeitrain_path = file_path + 'ijcai2016_koubei_train'
-    koubeitest_path = file_path + 'ijcai2016_koubei_test'
+    file_path = 'E:\IJCAI_competition\datasets\datasets'
+    taobao_path = file_path + '\ijcai2016_taobao'
+    merchantinfo_path = file_path + '\ijcai2016_merchant_info'
+    koubeitrain_path = file_path + '\ijcai2016_koubei_train'
+    koubeitest_path = file_path + '\ijcai2016_koubei_test'
 
-    result_path = '/home/wanghao/Document/tianchi/result/result.csv'
+    #result_path = '/home/wanghao/Document/tianchi/result/result.csv'
 
     merchant_budget = {}
     location_merchant = {}
-    location_merchant_nums = {}
+    user_merchant = {}
+    user = {}
+    merchant ={}
+    user_sim = {}
+    #seller_user_nums = {}
 
     def __init__(self):
         print "init"
@@ -56,34 +61,74 @@ class data :
 
         # Process the test file
 
-
-
-
     def outputdata(self):
         print "output the result to csv"
-
-    def get_location_merchant_nums(self):
-        # process train file
-        with open(self.koubeitrain_path) as f :
-            for line in f :
-                linelist = line.split(',')
-                location = linelist[2]
-                merchant = linelist[1]
-                if self.location_merchant_nums.has_key(location):
-                    merchant_nums = self.location_merchant_nums[location]
-                    if merchant_nums.has_key(merchant):
-                        merchant_nums[merchant] = merchant_nums[merchant] + 1
-                    else:
-                        merchant_nums[merchant] = 1
-                else:
-                    merchant_nums = {}
-                    merchant_nums[merchant] = 1
-                    self.location_merchant_nums[location] = merchant_nums
-
-        for dict in self.location_merchant_nums:
-            print dict,self.location_merchant_nums[dict]
-
 
     def setdatapath(self,foldpath):
 
         self.file_path = foldpath
+    
+    def input_train_data(self):
+        file = open(self.koubeitrain_path,'r')
+        i = 0
+        for line in file:
+            linelist = line.split(',')
+            self.user[linelist[0]] = i
+            i = i + 1
+            temp_merchant_dict = {}
+            if(self.user_merchant.has_key(linelist[0])):
+                if(self.user_merchant[linelist[0]].has_key(linelist[1])):
+                    self.user_merchant[linelist[0]][linelist[1]] = self.user_merchant[linelist[0]][linelist[1]] + 1
+                else:
+                    self.user_merchant[linelist[0]][linelist[1]] = 0
+            else:
+                temp_merchant_dict[linelist[1]] = 0
+                self.user_merchant[linelist[0]] = temp_merchant_dict
+            #self.user_merchant = sorted(self.user_merchant.iteritems(),key = lambda d:d[0])
+        print('number of user:%d'%len(self.user))
+        print('number of user_merchant:%d'%len(self.user_merchant))
+
+    def input_merchant_data(self):
+        file = open(self.merchantinfo_path,'r')
+        j = 0
+        for line in file:
+            linelist1 = line.split(',')
+            self.merchant[linelist1[0]] = j
+            j = j + 1
+        print('number of merchant:%d'%len(self.merchant))
+
+    def Mer_Sim(self,d1,d2):
+        keys1 = set(d1.keys())
+        keys2 = set(d2.keys())
+        return float((len(keys1&keys2)))/(len(keys1|keys2))
+
+    def Comp_User_Sim(self):
+        for usr_i in self.user_merchant:
+            mer_dict_i = self.user_merchant[usr_i]
+            self.user_sim[usr_i] = []
+            for usr_j in self.user_merchant:
+                mer_dict_j = self.user_merchant[usr_j]
+                if(self.Mer_Sim(mer_dict_i,mer_dict_j)>0.5):
+                    self.user_sim[usr_i].append(usr_j)
+        result = []
+        usr_count = []
+        for usr in self.user_sim:
+            usr_count.append(len(self.user_sim[usr]))
+            res = usr + ','
+            for usrs in self.user_sim[usr]:
+                res = res + str(usrs) + ':'
+            res = res[0:len(res)-1]
+        result.append(res)
+        csvfile = 'E:\IJCAI_competition\code\DataProcess\simi_usr.csv'
+        with open(csvfile,'wb') as f:
+            writer = csv.writer(f)
+            writer.writerows(result)
+
+        usr_count.sort(reversed = True)
+        print(usr_count[1:5])
+d = data()
+#d.input_traindata()
+  #  def input_taobao_data(self):
+  #      file = open(self.taobao_path,'r')
+d.input_train_data()
+d.Comp_User_Sim()
