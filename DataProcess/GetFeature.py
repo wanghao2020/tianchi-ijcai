1bc015365b0e451a0788f7e9022813b0dde7dc0d
goiter <wgf1109@qq.com>
2016-05-11 15:51:22 +0800

    getfeature&taobao_user_feature

--- DataProcess/GetFeature.py
+++ DataProcess/GetFeature.py
@@ -1,6 +1,7 @@
 import pickle
 import numpy as np
-
+import datetime
+import time
 #sample_path = 'E:\IJCAI_competition\datasets\datasets\model_train.csv'
 #label_path = 'E:\IJCAI_competition\datasets\datasets\model_label.csv'
 #test_path = 'E:\IJCAI_competition\datasets\datasets\model_test.csv'
@@ -13,6 +14,8 @@ class feature:
     location_merchant_nums = {}
     location_passenger_flow = {}
     location_merchant_regular_customer_nums = {}
+    user_merchant_datetime = {}
+    user_location = {}
     merchant_feature = {}
     user_feature = {}
     UM_feature = {}
@@ -22,9 +25,7 @@ class feature:
     def get_location_merchant_nums(self,dataset):
         with open(dataset) as f:
             for line in f:
-                linelist = line.split(',')
-                location = linelist[2]
-                merchant = linelist[1]
+                user,merchant,location,time = line.split(',')
                 if self.location_merchant_nums.has_key(location):
                     merchant_nums = self.location_merchant_nums[location]
                     if merchant_nums.has_key(merchant):
@@ -95,80 +96,159 @@ class feature:
                     # 3. count of merchants in which mer located
                     self.merchant_feature[(loc,mer)][3] = len(self.location_merchant_nums[loc])
                     # 6. the rate of regular customer
-                    self.merchant_feature[(loc,mer)][6] = self.location_merchant_regular_customer_nums[loc][mer][0] / self.location_merchant_regular_customer_nums[loc][mer][1]
+                    self.merchant_feature[(loc,mer)][6] = float(self.location_merchant_regular_customer_nums[loc][mer][0]) / self.location_merchant_regular_customer_nums[loc][mer][1]
 
                 # 4. passenger flow of mer / the sum of passenger flow local
-                self.merchant_feature[(loc,mer)][4] = self.merchant_feature[(loc,mer)][1]/self.merchant_feature[(loc,mer)][2]
-                # 4. average passenger flow local
-                self.merchant_feature[(loc,mer)][5] = self.merchant_feature[(loc,mer)][2]/self.merchant_feature[(loc,mer)][3]
+                self.merchant_feature[(loc,mer)][4] = float(self.merchant_feature[(loc,mer)][1]) / self.merchant_feature[(loc,mer)][2]
+                # 5. average passenger flow local
+                self.merchant_feature[(loc,mer)][5] = float(self.merchant_feature[(loc,mer)][2]) / self.merchant_feature[(loc,mer)][3]
 
+    #get user_location {user:[loc1,loc2]}
+    #get user_merchant_datetime {user:{merchant:[time1,time2...],..},...}
+    def get_user_merchant_datetime(self,dataset):
+        with open(dataset) as f:
+            for line in f:
+                user, merchant, location, time = line.split(',')
+                if not self.user_merchant_datetime.has_key(user):
+                    self.user_merchant_datetime[user] = {}
+                if not self.user_merchant_datetime[user].has_key(merchant):
+                    self.user_merchant_datetime[user][merchant] = []
+                format_time = datetime.datetime.strptime(time,'%Y-%m-%d')
+                self.user_merchant_datetime[user][merchant].append(format_time)
+                if not self.user_location.has_key(user):
+                    self.user_location[user] = []
+                if location not in self.user_location[user]:
+                    self.user_location[user].append(location)
 
-
-
-    #get user_feature {user:[x0,...,x15],...}
+    #get user_feature {user:[x0,...,x43],...}
     def get_user_feature(self,dataset):
         print "get user feature..."
         plk_file = open('/home/wanghao/Document/tianchi/datasets/user_feature_taobao.pkl','rb')
         user_feature_taobao = pickle.load(plk_file)
-        user_merchant_datetime = {}
-        with open(dataset) as f:
-            for line in f:
-                user, merchant, location, time = line.split(',')
-                # user_merchant_datetime {user:{merchant:[time1,time2...],..},...}
-                if not user_merchant_datetime.has_key(user):
-                    user_merchant_datetime[user] = {}
-                if not user_merchant_datetime[user].has_key(merchant):
-                    user_merchant_datetime[user][merchant] = []
-                user_merchant_datetime[user][merchant].append(time)
-        for user in user_merchant_datetime:
+        self.get_user_merchant_datetime(dataset)
+        for user in self.user_merchant_datetime:
             if not self.user_feature.has_key(user):
-                self.user_feature[user] = [0 for i in range(16)]
-
-            merchant_dict = user_merchant_datetime[user]
+                self.user_feature[user] = [0 for i in range(43)]
+            merchant_dict = self.user_merchant_datetime[user]
             active_days = []
-            different_mer = 0
+            repeat_mer_nums = 0
             for mer in merchant_dict:
-                # the count of user visited all merchants
+                # 0. the count of user visited all merchants
                 self.user_feature[user][0] += len(merchant_dict[mer])
                 active_days.extend(merchant_dict[mer])
-                different_mer += self.merchant_feature[mer][0]
-            # the count of user visited different merchants
+                if len(merchant_dict[mer]) > 1:
+                    repeat_mer_nums += 1
+            # 1. num of all visited merchants
             self.user_feature[user][1] = len(merchant_dict)
-            # user active days offline
-            self.user_feature[user][5] = len(set(active_days))
-            # the count of user visited all merchants/user active days offline
-            self.user_feature[user][10] = self.user_feature[user][0]/self.user_feature[user][5]
-            # the count of user visited different merchants/count of all merchants local
-            self.user_feature[user][11] = self.user_feature[user][1]/different_mer
+            # 9. user active days offline
+            self.user_feature[user][9] = len(set(active_days))
+            active_days.sort()
+            diff_days = []
+            for i in range(len(active_days)-1):
+                diff_days.append((active_days[i+1]-active_days[i]).days)
+            if len(diff_days) >0:
+                # 13. user max diff days offline
+                self.user_feature[user][13] = max(diff_days)
+                # 14. user min diff days offline
+                self.user_feature[user][14] = min(diff_days)
+                # 15. user avg diff days offline
+                self.user_feature[user][15] = float(sum(diff_days))/len(diff_days)
+            else:
+                self.user_feature[user][13] = -1
+                self.user_feature[user][14] = -1
+                self.user_feature[user][15] = -1
+
+            # 16. count of user visited location
+            self.user_feature[user][16] = len(self.user_location[user])
+
+            # 22. the count of user visited all merchants/user active days offline
+            self.user_feature[user][22] = float(self.user_feature[user][0]) / self.user_feature[user][9]
+            # num of merchants in loc where mer located
+            mer_loc_nums = 0
+            for loc in self.user_location[user]:
+                mer_loc_nums += len(self.location_merchant_nums[loc])
+            # 23. the number of user visited different merchants/count of all merchants local
+            self.user_feature[user][23] = float(self.user_feature[user][1]) / mer_loc_nums
+
+            # 36. num of repeat visited merchants / num of all visited merchants
+            self.user_feature[user][36] = float(repeat_mer_nums) / self.user_feature[user][1]
+
             if user_feature_taobao.has_key(user):
-                # user click number
+                # 2. user click number
                 self.user_feature[user][2] = user_feature_taobao[user][0]
-                # user buy number
+                # 3. user buy number
                 self.user_feature[user][3] = user_feature_taobao[user][1]
-                # user active days online
-                self.user_feature[user][4] = user_feature_taobao[user][4]
-                # number of user click different category
-                self.user_feature[user][6] = user_feature_taobao[user][2]
-                # number of user buy different category
-                self.user_feature[user][7] = user_feature_taobao[user][3]
-                # user buy number / user click number
-                if self.user_feature[user][2]:
-                    self.user_feature[user][8] = self.user_feature[user][3]/self.user_feature[user][2]
-                # buy num/click num (different category)
-                if self.user_feature[user][6]:
-                    self.user_feature[user][9] = self.user_feature[user][7]/self.user_feature[user][6]
-                # number of user click different category/active days online
-                self.user_feature[user][12] = self.user_feature[user][6]/self.user_feature[user][4]
-                # number of user buy different category/ active days online
-                self.user_feature[user][13] = self.user_feature[user][7]/self.user_feature[user][4]
-                # user click num/active days online
-                self.user_feature[user][14] = self.user_feature[user][2]/self.user_feature[user][4]
-                # user buy num/active days online
-                self.user_feature[user][15] = self.user_feature[user][3]/self.user_feature[user][4]
+                # 4. number of user click different category
+                self.user_feature[user][4] = user_feature_taobao[user][2]
+                # 5. number of user buy different category
+                self.user_feature[user][5] = user_feature_taobao[user][3]
+                # 6. number of user click different seller
+                self.user_feature[user][6] = user_feature_taobao[user][4]
+                # 7. number of user buy different seller
+                self.user_feature[user][7] = user_feature_taobao[user][5]
+                # 8 .user active days online
+                self.user_feature[user][8] = user_feature_taobao[user][6]
+                # 10. user max diff days online
+                self.user_feature[user][10] = user_feature_taobao[user][7]
+                # 11. user min diff days online
+                self.user_feature[user][11] = user_feature_taobao[user][8]
+                # 12. user avg diff days online
+                self.user_feature[user][12] = user_feature_taobao[user][9]
+
+                # 17. total click num
+                self.user_feature[user][17] = user_feature_taobao[user][16]
+                # 18. total buy num
+                self.user_feature[user][18] = user_feature_taobao[user][17]
+
+                # 19. buy num / click num (different item)
+                self.user_feature[user][19] = float(self.user_feature[user][3] + 1) / (self.user_feature[user][2] + 1)
+                # 20. buy num / click num (different category)
+                self.user_feature[user][20] = float(self.user_feature[user][5] + 1) / (self.user_feature[user][4] + 1)
+                # 21.buy num / click num (different seller)
+                self.user_feature[user][21] = float(self.user_feature[user][7] + 1) / (self.user_feature[user][6] + 1)
+
+                # 24. number of user click different category /active days online
+                self.user_feature[user][24] = float(self.user_feature[user][4]) / self.user_feature[user][8]
+                # 25. number of user buy different category / active days online
+                self.user_feature[user][25] = float(self.user_feature[user][5]) / self.user_feature[user][8]
+                # 26. number of user click different item / active days online
+                self.user_feature[user][26] = flaot(self.user_feature[user][2]) / self.user_feature[user][8]
+                # 27. number of user buy different item / active days online
+                self.user_feature[user][27] = float(self.user_feature[user][3]) / self.user_feature[user][8]
+                # 28. number of user click different seller / active days online
+                self.user_feature[user][28] = float(self.user_feature[user][6]) / self.user_feature[user][8]
+                # 29. number of user buy different seller / active days online
+                self.user_feature[user][29] = float(self.user_feature[user][7]) / self.user_feature[user][8]
 
 
+                # 30. num of repeat click item / all num of click item
+                self.user_feature[user][30] = float(user_feature_taobao[user][10] + 1) / (user_feature_taobao[user][0] + 1)
+                # 31. num of repeat buy item / all num of buy item
+                self.user_feature[user][31] = float(user_feature_taobao[user][11] + 1) / (user_feature_taobao[user][1] + 1)
+                # 32. num of repeat click category / all num of click category
+                self.user_feature[user][32] = float(user_feature_taobao[user][12] + 1) / (user_feature_taobao[user][2] + 1)
+                # 33. num of repeat buy category / all num of buy category
+                self.user_feature[user][33] = float(user_feature_taobao[user][13] + 1) / (user_feature_taobao[user][3] + 1)
+                # 34. num of repeat click seller / all num of click seller
+                self.user_feature[user][34] = float(user_feature_taobao[user][14] + 1) / (user_feature_taobao[user][4] + 1)
+                # 35. num of repeat buy seller / all num of buy seller
+                self.user_feature[user][35] = float(user_feature_taobao[user][15] + 1) / (user_feature_taobao[user][5] + 1)
 
-    #get UM_feature {[user,merchant]:[x0,...,x4],...}
+
+                # 37. number of user click different category / total click num
+                self.user_feature[user][37] = float(self.user_feature[user][4]) / user_feature_taobao[user][16]
+                # 38. number of user buy different category / total buy num
+                self.user_feature[user][38] = float(self.user_feature[user][5]) / user_feature_taobao[user][17]
+                # 39. number of user click different item / total click num
+                self.user_feature[user][39] = float(self.user_feature[user][2]) / user_feature_taobao[user][16]
+                # 40. number of user buy different item / total buy num
+                self.user_feature[user][40] = float(self.user_feature[user][3]) / user_feature_taobao[user][17]
+                # 41. number of user click different seller / total click num
+                self.user_feature[user][41] = float(self.user_feature[user][6]) / user_feature_taobao[user][16]
+                # 42. number of user buy different seller / total buy num
+                self.user_feature[user][42] = float(self.user_feature[user][7]) / user_feature_taobao[user][17]
+
+    #get UM_feature {[user,merchant]:[x0,...,x9],...}
     def get_user_merchant_feature(self,dataset):
         print "get user_merchant feature..."
         UM_time = {}
@@ -177,21 +257,45 @@ class feature:
                 user, merchant, location, time = line.split(',')
                 UM_pair = (user,merchant)
                 if not self.UM_feature.has_key(UM_pair):
-                    self.UM_feature[UM_pair] = [0,0,0,0]
-                # count of user visited merchant A
+                    self.UM_feature[UM_pair] = [0] * 9
+                # 0. count of user visited merchant A
                 self.UM_feature[UM_pair][0] += 1
                 if not UM_time.has_key(UM_pair):
                     UM_time[UM_pair] = []
-                UM_time[UM_pair].append(time)
+                format_time = datetime.datetime.strptime(time,'%Y-%m-%d')
+                UM_time[UM_pair].append(format_time)
         for key in self.UM_feature:
-            # count of user visited merchant A/count of user visited all merchants
-            #print "user",key[0]
-            #print "error:", self.user_feature[key[0]][0]
-            self.UM_feature[key][1] = self.UM_feature[key][0]/self.user_feature[key[0]][0]
-            # count of user visited merchant A/passenger flow of all merchants which merchant A located
-            self.UM_feature[key][2] = self.UM_feature[key][0]/self.merchant_feature[key[1]][0]
-            # days of user visited merchant A/active days offline
-            self.UM_feature[key][3] = len(set(UM_time[key]))/self.user_feature[key[0]][5]
+            # 1. days of user visited merchant A
+            self.UM_feature[key][1] = len(set(UM_time[key]))
+            diff_days = []
+            for i in range(len(UM_time[key])-1):
+                diff_days.append((UM_time[key][i+1]-UM_time[key][i]).days)
+            if len(diff_days) > 0:
+                # 2. max diff days
+                self.UM_feature[key][2] = max(diff_days)
+                # 3. min diff days
+                self.UM_feature[key][3] = min(diff_days)
+                # 4. avg diff days
+                self.UM_feature[key][4] = float(sum(diff_days))/len(diff_days)
+            else:
+                self.UM_feature[key][2] = -1
+                self.UM_feature[key][3] = -1
+                self.UM_feature[key][4] = -1
+
+            # 5. count of user visited merchant A / count of user visited all merchants
+            self.UM_feature[key][5] = float(self.UM_feature[key][0])/self.user_feature[key[0]][0]
+            mer_passenger_flow = 0
+            for loc in self.location_merchant_nums:
+                if key[1] in self.location_merchant_nums[loc]:
+                    mer_passenger_flow += len(self.location_merchant_nums[loc][key[1]])
+            # 6. count of user visited merchant A / passenger flow of merchant A
+            self.UM_feature[key][6] = float(self.UM_feature[key][0]) / mer_passenger_flow
+
+            # 7. days of user visited merchant A / user active days offline
+            self.UM_feature[key][7] = float(self.UM_feature[key][1]) / self.user_feature[key[0]][9]
+            # 8. count of user visited merchant A / user active days offline
+            self.UM_feature[key][8] = float(self.UM_feature[key][0]) / self.user_feature[key[0]][9]
+
 
     #get label [user,merchant]
     def get_label(self,dataset):
@@ -204,39 +308,22 @@ class feature:
 if __name__ == '__main__':
 
     f = feature()
-    f.get_merchant_feature(train_path)
+    f.get_location_merchant_feature(train_path)
     f.get_user_feature(train_path)
     f.get_user_merchant_feature(train_path)
     f.get_label(label_path)
-    sample = np.zeros((len(f.UM_feature),26+1))
+    sample = []
+    UML_pair = []
+    with open(train_path) as f:
+        for line in f:
+            user,merchant,loction,time = line.split(',')
+            if (user,merchant,loction) not in UML_pair:
+                UML_pair.append((user,merchant,location))
+                sam = []
+                sam.extend(f.merchant_feature[(loction,merchant)])
+                sam.extend(f.user_feature[user])
+                sam.extend(f.UM_feature[(user,merchant)])
+            UML_pair.append(sam)
 
-    i = 0
-    for key in f.UM_feature:
-        user = key[0]
-        merchant = key[1]
-        j = 0
-        for k in range(6):
-            sample[i][j] = f.merchant_feature[merchant][k]
-            j += 1
-            if key in f.label:
-                sample[i][j] = 1
-        for k in range(16):
-            sample[i][j] = f.user_feature[user][k]
-            j += 1
-            if key in f.label:
-                sample[i][j] = 1
-        for k in range(4):
-            sample[i][j] = f.UM_feature[key][k]
-            j += 1
-
-        # positive sample
-        if key in f.label:
-            sample[i][j] = 1
     outfile = open('/home/wanghao/Document/tianchi/datasets/sample.pkl','wb')
-
-        #if key in f.label:
-        #   sample[i][j] = 1
-
-    #outfile = open('E:\IJCAI_competition\datasets\datasets\sample.pkl','wb')
-
     pickle.dump(sample,outfile)
--- DataProcess/taobao_user_feature.py
+++ DataProcess/taobao_user_feature.py
@@ -1,44 +1,123 @@
 import pickle
+import datetime
+import time
+from itertools import islice
+taobao_path = 'E:\IJCAI_competition\datasets\datasets\Taobao_test.csv'
 
-taobao_path = 'E:\IJCAI_competition\datasets\datasets\ijcai2016_taobao'
-
+user_item = {}
 user_category ={}
+user_seller = {}
+user_action_nums = {}
 user_time = {}
+user_diff_time = {}
 user_feature = {}
 count = 1
 with open(taobao_path) as f:
-    for line in f:
+    for line in islice(f,1,None):
         print "count:", count
         count += 1
+        line = line.strip('\n')
         user,seller,item,category,action,time = line.split(',')
+        #print type(time)
+       # print "time:" , time
         if not user_feature.has_key(user):
-            user_feature[user] = [0,0,0,0,0]
+            user_feature[user] = [0] * 18
+        if not user_item.has_key(user):
+            user_item[user] = {}
+        if not user_item[user].has_key(item):
+            user_item[user][item] = [0,0]
         if not user_category.has_key(user):
             user_category[user] = {}
         if not user_category[user].has_key(category):
             user_category[user][category] = [0,0]
+        if not user_seller.has_key(user):
+            user_seller[user] = {}
+        if not user_seller[user].has_key(seller):
+            user_seller[user][seller] = [0,0]
+        if not user_action_nums.has_key(user):
+            user_action_nums[user] = [0,0]
         #print("action:",type(action),action)
         if action == '0':
-            user_feature[user][0] += 1
+            user_item[user][item][0] += 1
             user_category[user][category][0] += 1
+            user_seller[user][seller][0] += 1
+            user_action_nums[user][0] += 1
         else:
-            user_feature[user][1] += 1
+            user_item[user][item][1] += 1
             user_category[user][category][1] += 1
+            user_seller[user][seller][1] += 1
+            user_action_nums[user][1] += 1
         if not user_time.has_key(user):
             user_time[user] = []
-        user_time[user].append(time)
+        format_time = datetime.datetime.strptime(time,'%Y-%m-%d')
+        user_time[user].append(format_time)
+        user_time[user].sort()
 
-#get {user:[#click,#buy,#num of different category with click action,#num of different category with buy action,#active days online]}
+#get {user:[,,,,]}
 for user in user_feature:
+    for item in user_item[user]:
+        if user_item[user][item][0] > 0:
+            # 0. num of different item with click action
+            user_feature[user][0] += 1
+            if user_item[user][item][0] > 1:
+                # 10. num of repeat click different item
+                user_feature[user][10] += 1
+        if user_item[user][item][1] > 0:
+            # 1. num of different item with buy action
+            user_feature[user][1] += 1
+            if user_item[user][item][1] > 1:
+                # 11. num of repeat buy different item
+                user_feature[user][11] += 1
     for category in user_category[user]:
-        if user_category[user][category][0] > 1:
+        if user_category[user][category][0] > 0:
+            # 2. num of different category with click action
             user_feature[user][2] += 1
-        if user_category[user][category][1] > 1:
+            if user_category[user][category][0] > 1:
+                # 12. num of repeat click different category
+                user_feature[user][12] += 1
+        if user_category[user][category][1] > 0:
+            # 3. num of different category with buy action
             user_feature[user][3] += 1
+            if user_category[user][category][1] > 1:
+                # 13. num of repeat buy different category
+                user_feature[user][13] += 1
+    for seller in user_seller[user]:
+        if user_seller[user][seller][0] > 0:
+            # 4. num of different seller with click action
+            user_feature[user][4] += 1
+            if user_seller[user][seller][0] > 1:
+                # 14. num of repeat click different seller
+                user_feature[user][14] += 1
+        if user_seller[user][seller][1] > 0:
+            # 5. num of different seller with buy action
+            user_feature[user][5] += 1
+            if user_seller[user][seller][1] > 1:
+                # 15. num of repeat buy different seller
+                user_feature[user][15] += 1
     timelist = user_time[user]
-    user_feature[user][4] = len(set(timelist))
+    # 6. active days online
+    user_feature[user][6] = len(set(timelist))
+    if not user_diff_time.has_key(user):
+        user_diff_time[user] = []
+    for i in range(len(user_time[user])-1):
+        user_diff_time[user].append((user_time[user][i+1] - user_time[user][i]).days)
+    if len(user_diff_time[user]) > 0:
+        # 7. max diff days
+        user_feature[user][7] = max(user_diff_time[user])
+        # 8. min diff days
+        user_feature[user][8] = min(user_diff_time[user])
+        # 9. avg diff days
+        user_feature[user][9] = float(sum(user_diff_time[user])) / len(user_diff_time[user])
+    else:
+        user_feature[user][7] = -1
+        user_feature[user][8] = -1
+        user_feature[user][9] = -1
+    # 16. total click nums
+    user_feature[user][16] = user_action_nums[user][0]
+    # 17. total buy nums
+    user_feature[user][17] = user_action_nums[user][1]
 
-outfile = 'E:\IJCAI_competition\datasets\datasets\user_feature_taobao.pkl'
+outfile = 'E:\IJCAI_competition\datasets\datasets\user_feature_taobao_test.pkl'
 output = open(outfile,'wb')
 pickle.dump(user_feature,output)
 
